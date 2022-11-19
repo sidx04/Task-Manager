f61f217a7333bc6b17578eb60ed67d437e95e4f0
Onion-Man <siddpal04@gmail.com>
2022-11-19 21:32:54 +0530

    inital commit

---
+++ starter/app.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express()
+
+/**
+ ** routes */
+app.get('/hello', (req,res)=>{
+    res.send("Task-Manager")
+})
+
+const port=3000
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}...`);
+})
