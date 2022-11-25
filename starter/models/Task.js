cd9460eed56769e46f7e1f4b47c5cca5aaddd6ae
Onion-Man <siddpal04@gmail.com>
2022-11-26 00:15:37 +0530

    intro to create operation

--- starter/models/Task.js
+++ starter/models/Task.js
@@ -1,8 +1,24 @@
 const mongoose=require('mongoose')
 
 const TaskSchema=new mongoose.Schema({
-    name:String,
-    completed:Boolean
+    
+    /**
+     *`  var:{
+     *>     validators go in here
+     *`  }
+     */
+
+    name:{
+        type:String,
+        required:[true,'must provide data'],
+        trim:true,
+        maxLength:[20,'data cant exceed 20 characters']
+
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 })
 
 module.exports=mongoose.model('Task',TaskSchema)
