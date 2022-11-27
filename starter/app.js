2ad5e431bab13d99fb7e9825861571c5c4f9c5b0
Onion-Man <siddpal04@gmail.com>
2022-11-27 16:33:56 +0530

    final

--- starter/app.js
+++ starter/app.js
@@ -17,7 +17,8 @@ app.use(notFound)
 app.use(errorHandlerMiddleWare)
 
 //port
-const port=3000
+const port=process.env.PORT || 3000
+
 const start=async()=>{
     try {
         await connectDB(process.env.MONGO_URI)
